58018396161d30b7d323a385f8dfc4f950a55632
Elton Mozart <elton.mozart@gmail.com>
2024-04-13 19:24:23 -0300

    :test_tube: Finalização da segunda e terceira parte do desafio

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -24,13 +24,21 @@ class RegisterForm {
   clickSubmit() {
     this.elements.submitBtn().click();
   }
+
+  hitEnter() {
+    cy.focused().type('{enter}')
+  }
 }
 
 const registerForm = new RegisterForm();
 const colors = {
   errors: "rgb(220, 53, 69)",
-  success: "",
+  success: "rgb(222, 226, 230)",
 };
+
+const icons = {
+  success: "data:image/svg+xml,%3csvg xmlns='http://www.w3.org…27 1.2.7l-4 4.6c-.43.5-.8.4-1.1.1z'/%3e%3c/svg%3e"
+}
 
 describe("Image Registration", () => {
   describe("Submitting an image with invalid inputs", () => {
@@ -82,3 +90,116 @@ describe("Image Registration", () => {
     });
   });
 });
+
+describe("Submitting an image with valid inputs using enter key", () => {
+  describe("Given I am on the image registration page", () => {
+    after(() => {
+      cy.clearAllLocalStorage();
+    });
+
+    const input = {
+      title: "Alien BR",
+      url: "https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg",
+    };
+
+    it("Given I am on the image registration page", () => {
+      cy.visit("/");
+    });
+
+    it(`When I enter ${input.title} in the title field`, () => {
+      registerForm.typeTitle(input.title);
+    });
+
+    it('I should see a check icon in the title field', () => {
+      registerForm.elements.titleInput().should(([$input]) => {
+        const styles = window.getComputedStyle($input);
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.success)
+      })
+    })
+
+    it(`When I enter ${input.url} in the imageUrl field`, () => {
+      registerForm.typeUrl(input.url);
+    });
+
+    it('I should see a check icon in the imageUrl field', () => {
+      registerForm.elements.imageUrlInput().should(([$input]) => {
+        const styles = window.getComputedStyle($input);
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.success)
+      })
+    })
+
+    it('Then I can hit enter to submit the form', () => {
+      registerForm.hitEnter()
+      cy.wait(100)
+    })
+
+    it('And the list of registered images should be updated with the new item', () => {
+      cy.get('#card-list .card-img').should((elements) => {
+        const lastElement = elements[elements.length - 1]
+        const src = lastElement.getAttribute('src')
+        assert.strictEqual(src, input.url)
+      })
+    });
+
+    it('And the new item should be stored in the localStorage', () => {
+      cy.getAllLocalStorage().should((ls) => {
+        const currentLs = ls[window.location.origin]
+        const elements = JSON.parse(Object.values(currentLs))
+        const lastElement = elements[elements.length - 1]
+
+        assert.deepStrictEqual(lastElement, {
+          title: input.title,
+          imageUrl: input.url,
+        })
+
+      })
+    })
+
+    it('Then The inputs should be cleared', () => {
+      registerForm.elements.titleInput().should('have.value', '')
+      registerForm.elements.imageUrlInput().should('have.value', '')
+    })
+  });
+});
+
+describe('Refreshing the page after submitting an image clicking in the submit button', () => {
+  const input = {
+    title: 'Alien BRAZIL',
+    url: 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg',
+  }
+
+  after(() => {
+    cy.clearLocalStorage()
+  })
+
+  it('Given I am on the image registration page', () => {
+    cy.visit('/')
+  })
+
+  it(`Then I have submitted an image by clicking the submit button`, () => {
+    registerForm.typeTitle(input.title)
+    registerForm.typeUrl(input.url)
+    registerForm.clickSubmit()
+    cy.wait(100)
+  })
+
+  it(`When I refresh the page`, () => {
+    cy.reload()
+  })
+
+  it('Then I should still see the submitted image in the list of registered images', () => {
+    cy.getAllLocalStorage().should((ls) => {
+      const currentLs = ls[window.location.origin]
+      const elements = JSON.parse(Object.values(currentLs))
+      const lastElement = elements[elements.length - 1]
+
+      assert.deepStrictEqual(lastElement, {
+        title: input.title,
+        imageUrl: input.url,
+      })
+    })
+  })
+
+})
